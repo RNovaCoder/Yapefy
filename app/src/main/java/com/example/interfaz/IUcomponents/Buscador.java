dcdc31c35aa476efdccaca4f2817469e34fa4af4
RNovaCoder <ricardodanielvilla1@gmail.com>
2023-07-15 21:20:39 -0500

    Avance general

---
+++ app/src/main/java/com/example/interfaz/IUcomponents/Buscador.java
@@ -0,0 +1,11 @@
+package com.example.interfaz.IUcomponents;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class Buscador extends LinearLayout {
+
+    public Buscador (Context contexto){
+        super(contexto);
+    }
+}
